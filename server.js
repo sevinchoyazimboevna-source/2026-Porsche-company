35e0cdf3059eb818a1b2d6c30a842ef04c77ccfe
sevinchoy <sevinchoyazimboevna@gmail.com>
2026-04-20 22:05:01 +0500

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,33 @@
+console.log('Web serverni boshlash');
+const express = require("express");
+const app = express();
+const http = require("http");
+
+//1;Kirish codlari
+
+app.use(express.static("public")); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2 sessionga boglik kodlar
+//3views code
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+ 
+//4routing code
+
+app.get("/Angel", function ( req , res) {
+    res.end(`<h1 style="background: red ">Hello World Im ANGEL</h1>`);
+});
+
+app.get("/gold", function ( req , res) {
+    res.end(`<h1 style="background: red ">Golden moments</h1>`);
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});

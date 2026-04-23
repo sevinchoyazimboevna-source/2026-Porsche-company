b9218c8ffc07578e99dfcdca95c9e80225845f82
sevinchoy <sevinchoyazimboevna@gmail.com>
2026-04-23 15:52:19 +0500

    feat: develop author router

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log('Web serverni boshlash');
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs  = require("fs");
+
+let user;
+fs.readFile("databace/user.json", "utf8", (err, data) => {
+    if(err) {
+console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 //1;Kirish codlari
 
@@ -26,13 +36,17 @@ app.set("view engine", "ejs");
 // });
 
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
+    // console.log(req.body);
+    // res.json({test: "success"});
 });
 
 app.get('/', function(req, res) {
     res.render("harid");
 });
+
+app.get('/author', (req, res) => { 
+    res.render("author", {user: user});
+})
 
 
 
